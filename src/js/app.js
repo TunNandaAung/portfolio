bed9139065b1548b5435d82d1d61ab7dde08596e
TunNandaAung <tunnandaaung@outlook.com>
2020-01-12 21:54:48 +0630

    Add TransformContent Component

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import NavDrawer from './components/NavigationDrawer.vue'
 import ScrollLink from './components/ScrollLink.vue'
 import inViewport from 'in-viewport'
+import TransformContent from './components/TransformContent'
 
 window.Vue = Vue;
 
@@ -11,15 +12,21 @@ const app = new Vue({
 
     components: {
         NavDrawer,
-        ScrollLink
+        ScrollLink,
+        TransformContent
     },
     mounted() {
         let indicator = document.querySelector('#indicator')
-        console.log(indicator)
+        let contactTitle = document.querySelector('#contact-title')
+
         window.addEventListener('scroll', () => {
             !inViewport(
                 document.querySelector('#home')
             ) ? indicator.classList.add('is-floating') : indicator.classList.remove('is-floating');
+
+            // inViewport(
+            //     document.querySelector('#contact')
+            // ) ? console.log('transform') : contactTitle.classList.remove('transform');
 
         }, { passive: true });
     }
---
+++ src/js/components/TransformContent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :id="id">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "observe"],
+
+  mounted() {
+    let section = document.querySelector(`#${this.id}`);
+
+    let observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          section.classList.add("transform");
+        } else {
+          section.classList.remove("transform");
+        }
+      },
+      {
+        threshold: 0.25
+      }
+    );
+    observer.observe(document.querySelector(`#${this.observe}`));
+  }
+};
+</script>
+
+<style>
+</style>
