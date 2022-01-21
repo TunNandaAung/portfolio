7f1c75ab5e717a7779752a09b5cabc84090fcbac
TunNandaAung <tunnandaaung@outlook.com>
2022-01-21 21:34:47 +0630

    Tweak things

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import Alpine from "alpinejs";
 import "./style.css";
 import intersect from "@alpinejs/intersect";
+import LocomotiveScroll from "locomotive-scroll";
+
+window.onload = function () {
+  document.getElementById("title").classList.add("is-inview");
+};
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  multiplier: 1.5,
+});
 
 window.Alpine = Alpine;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,43 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "floating-diagonal": {
+          "0%,100%": {
+            transform: "translateY(-10px) translateX(10px)",
+          },
+          "50%": {
+            transform: "translateY(5px) translateX(-5px)",
+          },
+        },
+        floating: {
+          "0%,100%": {
+            transform: "translateY(-10px)",
+          },
+          "50%": {
+            transform: "translateY(10px)",
+          },
+        },
+        "floating-alt": {
+          "0%,100%": {
+            transform: "translateY(-10px)",
+          },
+          "50%": {
+            transform: "translateY(10px)",
+          },
+        },
+        "heart-pulse": {
+          "10%": { transform: "scale(1.2)" },
+        },
+      },
+      animation: {
+        floating: "floating 3s ease-in-out infinite",
+        "floating-alt": "floating-alt 2.3s ease-in-out infinite",
+        "floating-diagonal": "floating-diagonal 3s ease-in-out infinite",
+        "heart-pulse": "heart-pulse 1s infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
