5364ca075f6e3d8a2d921e78b56b7dfd657cf5b1
TunNandaAung <tunnandaaung@outlook.com>
2022-01-27 20:44:37 +0630

    Add meta tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./**/*.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx,html}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
